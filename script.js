3baa58c065bd320525bae976b168d3c6177dc300
andreyyarmola <andrey.p.yarmola@gmail.com>
2018-12-23 15:37:17 -0800

    working code with word generator

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-var letter = "A";
-alert (letter);
+function generateword() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  for (var i = 0; i < 7; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+    document.getElementById("businessName").innerHTML = text;
+
+  //return text;
+}
+
+console.log(generateword());
